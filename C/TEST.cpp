9a3e82a0c3d7990b31593529420bdca159a1c880
YoussefJoexx <34859600+YoussefJoexx@users.noreply.github.com>
2017-12-26 14:12:13 +0200

    Create TEST.cpp

---
+++ C/TEST.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+char x;
+int counter=0;
+
+while (true)
+{
+    
+
+
+x=getchar();
+if (x=='\n')
+break;
+counter++;
+}
+
+}/*
